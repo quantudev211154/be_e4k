591c3c47b4d3f73936012c6f3761dc2a7ce6450a
Pham Quan Tu <quantu211154@gmail.com>
2023-05-17 00:42:34 +0700

    Remove dist folder

--- dist/configs/middlewares/auth.middleware.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.checkAuth = void 0;
-function checkAuth(req, res, next) {
-    next();
-}
-exports.checkAuth = checkAuth;
--- dist/constants/db.constant.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DB_MAX_POOL_SIZE = void 0;
-exports.DB_MAX_POOL_SIZE = 10;
